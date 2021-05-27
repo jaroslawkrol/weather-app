84d9ff451428f39b3d00dcad2a1c59197b1c203a
Jaroslaw Krol <jaroslawkrol11@gmail.com>
2021-05-27 13:59:26 +0200

    feat: extract open api key to env variables

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,15 @@
 import axios from 'axios';
 import { Constants } from '../utils/constants';
+import { OPEN_WEATHER_KEY } from 'react-native-dotenv';
 
 const api = axios.create({
   baseURL: Constants.API.BASE_URL,
 });
 
 const get = <T>(url: string, params: any) => {
+  console.log(OPEN_WEATHER_KEY);
   return api.get<T>(url, {
-    params: { ...params, appid: '6177024b7a2f82fd304a24c2a041ccec' },
+    params: { ...params, appid: OPEN_WEATHER_KEY },
   });
 };
 
